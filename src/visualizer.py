d2ba13124932b6641dd7e1407da7de14fa571491
Benareyo <betelyohannes39@gmail.com>
2026-04-25 22:33:34 +0300

    feat: add interactive controls and filtering to app

--- src/visualizer.py
+++ src/visualizer.py
@@ -1,25 +1,16 @@
 import matplotlib.pyplot as plt
 import seaborn as sns
+import streamlit as st
 
 def plot_temperature(df):
-    """
-    Plot temperature distribution
-    """
     plt.figure()
     sns.histplot(df["temperature"], kde=True)
     plt.title("Temperature Distribution")
-    plt.xlabel("Temperature")
-    plt.ylabel("Frequency")
-    plt.show()
+    st.pyplot(plt)
 
 
 def plot_relationship(df):
-    """
-    Plot temperature vs humidity
-    """
     plt.figure()
     sns.scatterplot(x=df["temperature"], y=df["humidity"])
     plt.title("Temperature vs Humidity")
-    plt.xlabel("Temperature")
-    plt.ylabel("Humidity")
-    plt.show()
+    st.pyplot(plt)
